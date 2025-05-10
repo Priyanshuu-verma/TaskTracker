8bb14eda3b2c3462be2680e77dd80fc9687e2c6a
Priyanshuu-verma <priyanshuu0106@gmail.com>
2025-05-10 20:13:28 +0530

    ok

--- task-tracker-backend/index.js
+++ task-tracker-backend/index.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const frontendUrl = process.env.FRONTEND_URL;
-app.use(cors());
 
 app.options('*', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', `${frontendUrl}`); // Update with your frontend URL
@@ -17,6 +16,8 @@ app.options('*', (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(204);
 });
+
+app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
